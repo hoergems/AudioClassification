798405d4ad7c9ecc2745588fc74a3383c035a950
Marcus <hoergems@gmail.com>
2020-08-03 15:07:12 +1000

    New initial joint angles, updated readme

--- setup.sh
+++ setup.sh
@@ -5,14 +5,14 @@ export OPPT_RESOURCE_PATH="${OPPT_RESOURCE_PATH}:${SCRIPTDIR}/models:$SCRIPTDIR/
 export GAZEBO_MODEL_PATH="${GAZEBO_MODEL_PATH}:${SCRIPTDIR}/models:"
 
 # Find the local ROS_IP
-#addr=$(hostname --all-ip-addresses || hostname -I)
-#IFS=' ' read -ra ADDR <<< "$addr"
-#for i in "${ADDR[@]}"; do
-#    if [[ $i == "10.66.171"* ]]; then
-#       ip=$i     
-#       break
-#    fi      
-#done
+addr=$(hostname --all-ip-addresses || hostname -I)
+IFS=' ' read -ra ADDR <<< "$addr"
+for i in "${ADDR[@]}"; do
+    if [[ $i == "10.66.171"* ]]; then
+       ip=$i     
+       break
+    fi      
+done
 
-#export ROS_MASTER_URI=http://10.66.171.1:11311/
-#export ROS_IP=$ip
+export ROS_MASTER_URI=http://10.66.171.1:11311/
+export ROS_IP=$ip
